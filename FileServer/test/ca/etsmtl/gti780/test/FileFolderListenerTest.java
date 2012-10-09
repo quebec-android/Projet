e638b3f5a07dfac798779c2ea16e7e10d53c5550
cardoin <cedric.ardoin@gmail.com>
2012-10-09 19:49:07 -0400

    fuck you

---
+++ FileServer/test/ca/etsmtl/gti780/test/FileFolderListenerTest.java
@@ -0,0 +1,23 @@
+package ca.etsmtl.gti780.test;
+
+import static org.junit.Assert.*;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.After;
+import org.junit.Test;
+
+public class FileFolderListenerTest {
+	List<File> createdFiles = new ArrayList<File>();
+	
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
